f6e415f8f5a5c7c2db013ea8fced29de20d9243c
Kevin Liu Rodrigues <kkevin.liu@protonmail.com>
2022-11-07 12:56:30 -0300

    trying to make dap work

--- lua/config/colorschemes/tokyonight.lua
+++ lua/config/colorschemes/tokyonight.lua
@@ -17,22 +17,22 @@ require("tokyonight").setup({
       fg = prompt,
     }
     -- Uncomment these to make invisible
-    hl.TelescopeNormal = {
-      bg = c.bg_dark,
-      fg = c.fg_dark,
-    }
-    hl.TelescopeBorder = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-    hl.TelescopePreviewTitle = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-    hl.TelescopeResultsTitle = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
+    -- hl.TelescopeNormal = {
+    --   bg = c.bg_dark,
+    --   fg = c.fg_dark,
+    -- }
+    -- hl.TelescopeBorder = {
+    --   bg = c.bg_dark,
+    --   fg = c.bg_dark,
+    -- }
+    -- hl.TelescopePreviewTitle = {
+    --   bg = c.bg_dark,
+    --   fg = c.bg_dark,
+    -- }
+    -- hl.TelescopeResultsTitle = {
+    --   bg = c.bg_dark,
+    --   fg = c.bg_dark,
+    -- }
   end,
 })
 vim.cmd([[colorscheme tokyonight-moon]])
--- lua/config/dap/python.lua
+++ lua/config/dap/python.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.setup(_)
   require("dap-python").setup("python3", {})
+  print("dap-python REQUIRED!")
 end
 
 return M
