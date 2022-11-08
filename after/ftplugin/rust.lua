3e0fcbedcc892cd27b8f30101d947cc208e065fe
Kevin Liu Rodrigues <kkevin.liu@protonmail.com>
2022-11-08 10:35:35 -0300

    Move codelldb dap configuration to ftplugin file

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,1 +1,28 @@
-print("Rust file loaded!")
+local dap = require("dap")
+require("config.dap").setup()
+
+dap.adapters.codelldb = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = vim.fn.stdpath("data") .. "/mason/packages/codelldb/codelldb",
+    args = { "--port", "${port}" },
+  },
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = true,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp
+
+dap.configurations.rust = dap.configurations.cpp
