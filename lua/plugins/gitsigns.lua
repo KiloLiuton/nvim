983c166029122705dc2579284f32f6017d12a7dd
Kevin Liu Rodrigues <x-krodrigues@zoox.com>
2026-07-22 10:45:02 -0300

    add next/prev hunk keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,7 +3,8 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require("gitsigns").setup({
+			local gitsigns = require("gitsigns")
+			gitsigns.setup({
 				signs = {
 					add = { text = "█" },
 					-- change = { text = "█" },
@@ -18,6 +19,26 @@ return {
 
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview hunk" })
 			vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Blame line" })
+			-- Jump between changes using ]c and [c
+			vim.keymap.set("n", "]c", function()
+				if vim.wo.diff then
+					return "]c"
+				end
+				vim.schedule(function()
+					gitsigns.nav_hunk("next")
+				end)
+				return "<Ignore>"
+			end, { expr = true, desc = "Next Git change" })
+
+			vim.keymap.set("n", "[c", function()
+				if vim.wo.diff then
+					return "[c"
+				end
+				vim.schedule(function()
+					gitsigns.nav_hunk("prev")
+				end)
+				return "<Ignore>"
+			end, { expr = true, desc = "Previous Git change" })
 		end,
 	},
 }
