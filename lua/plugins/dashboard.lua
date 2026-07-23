e9b3b476e2edcbb2fc98d3b0326cab3a82db1768
Kevin Liu Rodrigues <x-krodrigues@zoox.com>
2026-07-23 11:48:04 -0300

    Add dashboard with an option to re-open all closed buffers in the last session

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre",
+		opts = {},
+	},
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons", "amansingh-afk/milli.nvim", "nvim-telescope/telescope.nvim" },
+		},
+		opts = function()
+			local splash = require("milli").load({ splash = "finger" })
+			return {
+				theme = "doom",
+				config = {
+					header = splash.frames[1], -- seed header with frame 0
+					center = {
+						{ icon = "🔭", desc = "Find File", key = "f", action = "Telescope find_files" },
+						{ icon = " ", desc = "Restore buffers", key = "r", action = "lua require('persistence').load()", },
+						{ icon = "  ", desc = "Quit", key = "q", action = "qa" },
+					},
+				},
+			}
+		end,
+		config = function(_, opts)
+			require("dashboard").setup(opts)
+			require("milli").dashboard({ splash = "finger", loop = true })
+		end,
+	},
+	{
+		"amansingh-afk/milli.nvim",
+		lazy = false,
+		config = function()
+			local milli = require("milli")
+			milli.dashboard({ splash = "fire", loop = true })
+		end,
+	},
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
 	},
 	config = function()
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<C-f>", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.current_buffer_fuzzy_find, { desc = "Buffer fuzzy find" })
 	end,
